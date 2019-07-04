4fba5d564856a3d7224bbdee9e9080da53057db8
TURKCELL\ext02d13865 <email@turkcell.com.tr>
2019-07-04 17:54:28 +0300

    deneme

--- signs/image_resizer.py
+++ signs/image_resizer.py
@@ -1,10 +1,10 @@
 import cv2
 import glob
-filename = '33_'
+filename = 'stop_'
 counter = 0
 params = " 1 0 0 25 25"
 with open('info.lst', 'w') as info_file:
-    for img in glob.glob("./dataset/33/*.png"):
+    for img in glob.glob("./dataset/14/*.png"):
         image = cv2.imread(img)
         resized_image = cv2.resize(image,(25,25),interpolation=cv2.INTER_AREA)
         final_file_name = filename+  str(counter)+".png"
@@ -13,16 +13,5 @@ with open('info.lst', 'w') as info_file:
         info_file.write(final_file_name + params +'\n')
     info_file.close()
 
-filename = '34_'
-counter = 0
-with open('info-left.lst', 'w') as info_file:
-    for img in glob.glob("./dataset/34/*.png"):
-        image = cv2.imread(img)
-        resized_image = cv2.resize(image,(25,25),interpolation=cv2.INTER_AREA)
-        final_file_name = filename+  str(counter)+".png"
-        cv2.imwrite(final_file_name,resized_image)
-        counter+=1
-        info_file.write(final_file_name + params +'\n')
-    info_file.close()
 
 
--- signs/recognizer.py
+++ signs/recognizer.py
@@ -1,21 +1,32 @@
 import cv2
-import numpy as np
 cascade_right = cv2.CascadeClassifier('data/cascade.xml')
 cascade_left = cv2.CascadeClassifier('data_left/cascade.xml')
+cascade_stop = cv2.CascadeClassifier('data_stop/cascade.xml')
 cap  = cv2.VideoCapture(0)
+fontscale = 0.8
+color = (255, 0, 0)
+fontface = cv2.FONT_HERSHEY_PLAIN
 while True:
     ret ,img = cap.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     sign_right = cascade_right.detectMultiScale(gray)
     sign_left =  cascade_left.detectMultiScale(gray)
-    for (x,y,w,h) in sign_right:
-        cv2.rectangle(img, (x,y), (x+w,y+h),(255,0,0),2)
+    sign_stop = cascade_stop.detectMultiScale(gray)
+    for (x,y,w,h) in sign_stop:
+        cv2.rectangle(img, (x,y), (x+w,y+h),(0,255,0),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = img[y:y+h,x:x+w]
+        cv2.putText(img,'stop', (x, y), fontface, fontscale, color)
     for (x, y, w, h) in sign_left:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
+        cv2.putText(img,'left_turn', (x, y), fontface, fontscale, color)
+    for (x, y, w, h) in sign_right:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        cv2.putText(img,'right_turn', (x, y), fontface, fontscale, color)
 
     cv2.imshow('img',img)
     k = cv2.waitKey(30) &0xff
