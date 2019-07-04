d28f72c31f4c65040774a0f35464f584b6d821a4
Mert Lostar <mertlostar@gmail.com>
2019-07-04 11:07:02 +0300

    2 Class classification xmls

--- signs/image_resizer.py
+++ signs/image_resizer.py
@@ -13,4 +13,16 @@ with open('info.lst', 'w') as info_file:
         info_file.write(final_file_name + params +'\n')
     info_file.close()
 
+filename = '34_'
+counter = 0
+with open('info-left.lst', 'w') as info_file:
+    for img in glob.glob("./dataset/34/*.png"):
+        image = cv2.imread(img)
+        resized_image = cv2.resize(image,(25,25),interpolation=cv2.INTER_AREA)
+        final_file_name = filename+  str(counter)+".png"
+        cv2.imwrite(final_file_name,resized_image)
+        counter+=1
+        info_file.write(final_file_name + params +'\n')
+    info_file.close()
+
 
--- signs/recognizer.py
+++ signs/recognizer.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
-cascade = cv2.CascadeClassifier('data/cascade.xml')
+cascade_right = cv2.CascadeClassifier('data/cascade.xml')
+cascade_left = cv2.CascadeClassifier('data_left/cascade.xml')
 cap  = cv2.VideoCapture(0)
 while True:
     ret ,img = cap.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sign = cascade.detectMultiScale(gray)
-    for (x,y,w,h) in sign:
+    sign_right = cascade_right.detectMultiScale(gray)
+    sign_left =  cascade_left.detectMultiScale(gray)
+    for (x,y,w,h) in sign_right:
         cv2.rectangle(img, (x,y), (x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = img[y:y+h,x:x+w]
+    for (x, y, w, h) in sign_left:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
     cv2.imshow('img',img)
     k = cv2.waitKey(30) &0xff
     if k ==27:
