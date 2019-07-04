deff809d2e16caf683f54ec95b058ee4c42388db
mchtalkn <mucahit.a.yorganci@gmail.com>
2019-07-04 20:28:40 +0300

    video record added

--- signs/recognizer.py
+++ signs/recognizer.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 cascade_right = cv2.CascadeClassifier('data/cascade.xml')
 cascade_left = cv2.CascadeClassifier('data_left/cascade.xml')
 cascade_stop = cv2.CascadeClassifier('data_stop/cascade.xml')
@@ -10,7 +11,9 @@ cap.set(cv2.cv.CV_CAP_PROP_FPS,24)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,180)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,180)
 ret ,img = cap.read()
-while True:
+imgarr=[]
+start_time=time.time()
+while time.time()-start_time<10:
     #response = "No detection"
     ret ,img = cap.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -37,10 +40,14 @@ while True:
         cv2.putText(img,'right_turn', (x, y), fontface, fontscale, color)
     #if response is not "No detection":
      #   print(response)    
-    cv2.imshow('img',img)
-    
+    #cv2.imshow('img',img)
+    imgarr.append(img)
     k = cv2.waitKey(30) &0xff
     if k ==27:
         break
+out = cv2.VideoWriter('project.avi',cv2.cv.CV_FOURCC('M','J','P','G'), 24, (180,180))
+for im in imgarr:
+    out.write(im)
+out.release()
 cap.release()
 cv2.destroyAllWindows()
