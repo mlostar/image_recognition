caebfb162cbeccb9fe5cb95c6792984033dfd9f3
Mücahit Yorgancı <mucahit.a.yorganci@gmail.com>
2019-07-25 15:55:15 +0300

    bug fix

--- pi_to_arduino/uart-signs.py
+++ pi_to_arduino/uart-signs.py
@@ -17,11 +17,12 @@ def _Pi_Serial_Catch():
         current_byte=serial0.read()
         print(current_byte)
         if(current_step==len(TRAFFIC_SIGN_BYTES)):
-            sign_result=which_sign(int(current_byte))
+            sign_result=which_sign(current_byte[0])
             message = b'\xAA\x44\x1C\x12\x01'+sign_result
+            serial0.write(message)
             #print(message)
             current_step=0
-        if current_byte==TRAFFIC_SIGN_BYTES[current_step]:
+        if current_byte[0]==TRAFFIC_SIGN_BYTES[current_step]:
             current_step += 1
         else:
             current_step=0
